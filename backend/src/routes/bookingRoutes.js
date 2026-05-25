ce3e38e6bd657bd33aa886c73347991472bfe774
gitar14 <gitaristya14@gmail.com>
2026-05-25 21:44:00 +0000

    Fix syntax error in bookingRoutes

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -1,17 +1,12 @@
-kconst express = require('express');
+const express = require('express');
 const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-// Queue & utilities
 router.get('/queue', bookingController.getQueue);
 router.get('/today', bookingController.getTodayBookings);
-
-// Workflow
 router.put('/:id/start', bookingController.startBooking);
 router.put('/:id/finish', bookingController.finishBooking);
-
-// CRUD
 router.get('/', bookingController.getAllBookings);
 router.post('/', bookingController.createBooking);
 
